75050026e470ed94ee6ed880019ec673053089bd
s21273 <s21273@pjwstk.edu.pl>
2021-12-15 00:03:04 +0100

    task 10

--- Task10/server.js
+++ Task10/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname + "/public/"));
-// send the user to index html
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
